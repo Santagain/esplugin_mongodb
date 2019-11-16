b0cd358bde6f8f8672466d1cb57321ed5ab5a8ea
Dione Batista <dione1993@hotmail.com>
2019-11-16 19:46:16 -0300

    Upload

--- server.lua
+++ server.lua
@@ -2,22 +2,30 @@
 -- set es_enableCustomData 1
 
 AddEventHandler('es_db:doesUserExist', function(identifier, callback)
-	MySQL.Async.fetchAll('SELECT 1 FROM users WHERE `identifier`=@identifier;', {identifier = identifier}, function(users)
-		if users[1] then
+	exports.DatabaseAPI:findOne({ collection = "users", query = { identifier = identifier } }, function (success, users)
+		if not success then
+			print("ERRO AO VERIFICAR SE O USUARIO EXISTE: "..tostring(users))
+			return
+		end
+        if users[1] then
 			callback(true)
-		else
+        else
 			callback(false)
 		end
 	end)
 end)
-
+ 
 AddEventHandler('es_db:retrieveUser', function(identifier, callback)
 	local Callback = callback
-	MySQL.Async.fetchAll('SELECT * FROM users WHERE `identifier`=@identifier;', {identifier = identifier}, function(users)
-		if users[1] then
-			Callback(users[1])
-		else
-			Callback(false)
+	exports.DatabaseAPI:findOne({ collection = "users", query = { identifier = identifier } }, function (success, users)
+		if not success then
+			print("ERRO AO OBTER DATA DO USUARIO VIA STEAMHEX: "..tostring(users))
+			return
+		end
+        if users[1] then
+			callback(users[1])
+        else
+			callback(false)
 		end
 	end)
 end)
@@ -27,39 +35,64 @@ AddEventHandler('es_db:createUser', function(identifier, license, cash, bank, ca
 		identifier = identifier,
 		money = cash or 0,
 		bank = bank or 0,
-		license = license,
+		xp = 0,
+		skin = null,
+		job = "unemployed",
+		job_grade = 0,
+		loadout =  null,
+		position = null,
+		status =  null,
+		firstname = "",
+		lastname = "",
+		dateofbirth = "=",
+		sex = "M",
+		height = "",
+		last_property = null,
+		pet = null,
+		jail = 0,
+		Pin = 1234,
+		warrant = "No",
+		skills = null,
+		phone_number = null,
+		tattoos = null,
+		is_dead = 0,
+		license = license or null,
 		group = 'user',
 		permission_level = 0
 	}
-
-	MySQL.Async.execute('INSERT INTO users (`identifier`, `money`, `bank`, `group`, `permission_level`, `license`) VALUES (@identifier, @money, @bank, @group, @permission_level, @license);',
-	{
-		identifier = user.identifier,
-		money = user.money,
-		bank = user.bank,
-		permission_level = user.permission_level,
-		group = user.group,
-		license = user.license
-	}, function(rowsChanged)
-		callback()
-	end)
+    exports.DatabaseAPI:insertOne({ collection="users", document = user }, function (success, result, insertedIds)
+        if not success then
+            print("[MongoDB] [ESBridge] ERRO AO CRIAR USUARIO: "..tostring(result))
+            return
+        end
+        print("[MongoDB] [ESBridge] USUARIO CRIADO. ID UNICA: "..tostring(insertedIds[1]))
+        callback()
+    end)
 end)
 
 AddEventHandler('es_db:retrieveLicensedUser', function(license, callback)
-	MySQL.Async.fetchAll('SELECT * FROM users WHERE `license`=@license;', {license = license}, function(users)
-		if users[1] then
+	exports.DatabaseAPI:findOne({ collection = "users", query = { license = license } }, function (success, users)
+		if not success then
+			print("[MongoDB] [ESBridge] ERRO AO OBTER DATA DO USUARIO VIA ROCKSTAR LICENSE: "..tostring(users))
+			return
+		end
+        if users[1] then
 			callback(users[1])
-		else
+        else
 			callback(false)
 		end
 	end)
 end)
 
 AddEventHandler('es_db:doesLicensedUserExist', function(license, callback)
-	MySQL.Async.fetchAll('SELECT 1 FROM users WHERE `license`=@license;', {license = license}, function(users)
-		if users[1] then
+	exports.DatabaseAPI:findOne({ collection = "users", query = { license = license } }, function (success, users)
+		if not success then
+			print("[MongoDB] [ESBridge] ERRO AO OBTER DATA DO USUARIO VIA ROCKSTAR LICENSE: "..tostring(users))
+			return
+		end
+        if users[1] then
 			callback(true)
-		else
+        else
 			callback(false)
 		end
 	end)
@@ -67,34 +100,17 @@ end)
 
 AddEventHandler('es_db:updateUser', function(identifier, new, callback)
 	Citizen.CreateThread(function()
-		local updateString = ''
 		local params = {identifier = identifier}
-
-		local length = tLength(new)
-		local cLength = 1
+		local obj = {}
 		for k,v in pairs(new) do
 			if (type(k) == 'string') then
-				updateString = updateString .. '`' .. k .. '`=@' .. k
 				params[k] = v
-				if cLength < length then
-					updateString = updateString .. ', '
-				end
+				obj[k] = v	
 			end
-			cLength = cLength + 1
 		end
-
-		MySQL.Async.execute('UPDATE users SET ' .. updateString .. ' WHERE `identifier`=@identifier', params, function(rowsChanged)
-			if callback then
-				callback(true)
-			end
-		end)
+		exports.DatabaseAPI:updateOne({ collection="users", query = { identifier = identifier }, update = { ["$set"] = obj } })
+		if callback then
+			callback(true)
+		end
 	end)
 end)
-
-function tLength(t)
-	local l = 0
-	for k,v in pairs(t)do
-		l = l + 1
-	end
-	return l
-end
